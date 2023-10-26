0120eaec0b0972da141891b323df49b27cbc966b
Yohei Okabayashi <yohei.kuro48@gmail.com>
2023-10-27 07:07:28 +0900

    ADD: sample batch

---
+++ sample-batch/src/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def run(raise_error: int = 0) -> None:
+    print("Hello World from main.py")
+    print(np.__version__)
+    if raise_error > 0:
+        raise RuntimeError("Error from main.py by raise_error=True")
+
+if __name__ == "__main__":
+    run()
